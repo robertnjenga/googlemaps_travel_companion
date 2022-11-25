638630c89cf05e5cf0a6a627c447d6af635a2a91
robert_codes <rnnjenga@gmail.com>
2022-11-25 12:45:07 -0600

    initial commit

---
+++ src/api/index.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          'X-RapidAPI-Key':
+            process.env.REACT_APP_RAPIDAPI_KEY,
+          'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    const { data } = await axios.get(
+      'https://rapidweather.p.rapidapi.com/data/2.5/weather',
+      {
+        params: {
+          lat: lat,
+          lon: lng,
+        },
+        headers: {
+          'X-RapidAPI-Key':
+          process.env.REACT_APP_RAPIDAPI_KEY,
+          'X-RapidAPI-Host': 'rapidweather.p.rapidapi.com'
+        },
+      }
+    );
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
