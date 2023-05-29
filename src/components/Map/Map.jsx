025733aa3a334e309124447f755773a5df8dbe1b
robert_codes <rnnjenga@gmail.com>
2023-05-29 09:27:09 -0700

    modified Map.jsx to fix errors

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -18,6 +18,36 @@ const Map = ({
   const classes = useStyles();
   const isDesktop = useMediaQuery('(min-width: 600px)');
 
+  const Marker = ({ place }) => {
+    return (
+      <div className={classes.markerContainer}>
+        {!isDesktop ? (
+          <LocationOnOutlinedIcon color="primary" fontSize="large" />
+        ) : (
+          <Paper elevation={3} className={classes.paper}>
+            <Typography
+              className={classes.typography}
+              variant="subtitle2"
+              gutterBottom
+            >
+              {place.name}
+            </Typography>
+            <img
+              src={
+                place.photo
+                  ? place.photo.images.large.url
+                  : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
+              }
+              className={classes.pointer}
+              alt={place.name}
+            />
+            <Rating size="small" value={Number(place.rating)} readOnly />
+          </Paper>
+        )}
+      </div>
+    );
+  };
+
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
@@ -26,7 +56,11 @@ const Map = ({
         center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          styles: mapStyles,
+        }}
         onChange={(e) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
@@ -34,40 +68,20 @@ const Map = ({
         onChildClick={(child) => setChildClicked(child)}
       >
         {places?.map((place, i) => (
-          <div
-            className={classes.markerContainer}
+          <Marker
+            place={place}
+            key={i}
             lat={Number(place.latitude)}
             lng={Number(place.longitude)}
-            key={i}
-          >
-            {!isDesktop ? (
-              <LocationOnOutlinedIcon color="primary" fontSize="large" />
-            ) : (
-              <Paper elevation={3} className={classes.paper}>
-                <Typography
-                  className={classes.typography}
-                  variant="subtitle2"
-                  gutterBottom
-                >
-                  {place.name}
-                </Typography>
-                <img
-                  src={
-                    place.photo
-                      ? place.photo.images.large.url
-                      : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
-                  }
-                  className={classes.pointer}
-                  alt={place.name}
-                />
-                <Rating size="small" value={Number(place.rating)} readOnly />
-              </Paper>
-            )}
-          </div>
+          />
         ))}
         {weatherData?.list?.map((data, i) => (
           <div key={i} lat={data.coord.lat} lng={data.coord.long}>
-            <img height={100} src={`https://rapidweather.com/img/${data.current.weather[0].icon}.png`} alt={''}/>
+            <img
+              height={100}
+              src={`https://rapidweather.com/img/${data.current.weather[0].icon}.png`}
+              alt={''}
+            />
           </div>
         ))}
       </GoogleMapReact>
